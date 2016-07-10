10c4de364dced51902d9ad9b517794e11b805d36
Obed lorisson <obed.lorisson@gmail.com>
2016-07-10 11:33:18 -0400

    fix sign issue

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -3,6 +3,25 @@ class ApplicationController < ActionController::Base
   # For APIs, you may want to use :null_session instead.
   protect_from_forgery with: :exception
 
+
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up)        { |u| u.permit(:username, :email, :password, :password_confirmation, :remember_me) }
+    devise_parameter_sanitizer.for(:sign_in)        { |u| u.permit(:login, :username, :email, :password, :remember_me) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:username, :email, :password, :password_confirmation, :current_password,:avatar) }
+  end
+
+
+  protected
+
+  def after_sign_in_path_for(resource)
+     dashboard_path # Or :prefix_to_your_route
+  end
+
+  def after_sign_up_path_for(resource)
+     dashboard_path # Or :prefix_to_your_route
+  end
+
+
   private
   
   #-> Prelang (user_login:devise)
--- config/routes.rb
+++ config/routes.rb
@@ -1,7 +1,20 @@
 Rails.application.routes.draw do
-  get 'landings/index'
 
-  devise_for :users
+  get "index" =>'landings#index'
+  get "dashboard" => "landings#dashboard"
+
+  devise_for :users, controllers: {registrations: "users/registrations", sessions: "users/sessions", passwords: "users/passwords", omniauth_callbacks: "users/omniauth_callbacks"}, skip: [:sessions, :registrations]
+  devise_scope :user do
+    get    "login"   => "users/sessions#new",         as: :new_user_session
+    post   "login"   => "users/sessions#create",      as: :user_session
+    delete "signout" => "users/sessions#destroy",     as: :destroy_user_session
+    
+    get    "signup"  => "users/registrations#new",    as: :new_user_registration
+    post   "signup"  => "users/registrations#create", as: :user_registration
+    put    "signup"  => "users/registrations#update", as: :update_user_registration
+    get    "account" => "users/registrations#edit",   as: :edit_user_registration
+  end
+
   devise_for :admin_users, ActiveAdmin::Devise.config
   ActiveAdmin.routes(self)
   # The priority is based upon order of creation: first created -> highest priority.
