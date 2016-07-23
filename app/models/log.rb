1d0e8679ac8000f48e5f7e367bf2456bd9511dea
Obed lorisson <obed.lorisson@gmail.com>
2016-07-22 21:20:10 -0400

    fix csv upload

--- app/models/log.rb
+++ app/models/log.rb
@@ -1,17 +1,19 @@
 class Log < ActiveRecord::Base
   belongs_to :user
 
-	def self.import(file)
-	  allowed_attributes = ["open", "close", "price", "pairs", "lot", "sl", "tp", "direction", "exit", "risk_reward", "win_loss", "loss", "profit", "loss_percent", "profit_percent", "balance", "strategy", "notes", "user_id"]
-	  spreadsheet = open_spreadsheet(file)
-	  header = spreadsheet.row(1)
-	  (2..spreadsheet.last_row).each do |i|
+
+    def self.import(file)
+	    spreadsheet = open_spreadsheet(file)
+	    header = spreadsheet.row(1)
+	   (2..spreadsheet.last_row).each do |i|
 	    row = Hash[[header, spreadsheet.row(i)].transpose]
 	    log = find_by_id(row["id"]) || new
-	    log.attributes = row.to_hash.select { |k,v| allowed_attributes.include? k }
+	    log.attributes = row.to_hash.slice(*Log.attribute_names())
 	    log.save!
 	  end
-	end
+    end
+
+	
 
     def self.open_spreadsheet(file)
 	  case File.extname(file.original_filename)
@@ -22,9 +24,13 @@ class Log < ActiveRecord::Base
 	  end
 	end
 
+=begin
 	
-	def self.accessible_attributes
+    def self.accessible_attributes
 	 [:open, :close, :price, :pairs, :lot, :sl, :tp, :direction, :exit, :risk_reward, :win_loss, :loss, :profit, :loss_percent, :profit_percent, :balance, :strategy, :notes, :user_id]
 	end
+	
+=end
+	
 
 end
